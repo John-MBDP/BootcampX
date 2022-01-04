d1f3129255672b349060fca9acbc937a3dec8d93
John <johnartuz@hotmail.com>
2022-01-03 23:50:18 -0500

    Fix bugs

--- 3_queries_group-by/6_avg_low_completion_vs_estimated_time.sql
+++ 3_queries_group-by/6_avg_low_completion_vs_estimated_time.sql
@@ -1,10 +1,8 @@
-
-SELECT students.name AS student,
-AVG(assignment_submissions.duration) AS average_assignment_duration,
-AVG(assignments.duration) AS average_estimated_duration
-FROM assignment_submissions
-JOIN students ON students.id = assignment_submissions.student_id
+SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM assignment_submissions 
+JOIN students ON students.id=student_id
 JOIN assignments ON assignment_submissions.assignment_id = assignments.id
 WHERE students.end_date IS NULL
 GROUP BY students.name
-ORDER BY AVG(assignment_submissions.duration) ASC;
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration ASC;
