ad0163fa874422c3d98eb9a12ce2f2166e379fcb
John <johnartuz@hotmail.com>
2022-01-05 21:09:50 -0500

    Implement SQL prevention

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,27 +7,19 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-// pool
-//   .query(
-//     `
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `
-//   )
-//   .then((res) => {
-//     console.log(res.rows);
-//   })
-//   .catch((err) => console.error("query error", err.stack));
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
+//Prevents SQL injection
 pool
   .query(
     `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `
   )
   .then((res) => {
--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+//Prevents SQL injection
 pool
   .query(
     `
@@ -15,7 +18,7 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || "JUL02"}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
 `
   )
@@ -23,4 +26,5 @@ ORDER BY teacher;
     res.rows.forEach((row) => {
       console.log(`${row.cohort}: ${row.teacher}`);
     });
-  });
+  })
+  .catch((err) => console.error("query error", err.stack));
